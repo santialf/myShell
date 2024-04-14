1d903d1b081ce6dbd73f6fe1c1434dcfa1a16ce1
santialf <91532972+santialf@users.noreply.github.com>
2024-04-14 10:03:13 +0100

    spmm 1d reports

---
+++ simula spmm 1D ncu report/ncu_report_1d_rcm.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#SBATCH -p a100q # partition (queue)
+#SBATCH -t 0-15:00 # time limit (D-HH:MM)
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+#SBATCH  --gpus-per-node 1
+
+export PATH=$PATH:/usr/local/cuda/bin
+
+nvidia-smi
+
+nvcc --version
+make 
+
+# Define the path to the directory containing the matrices
+MATRIX_DIR="/global/D1/homes/james/sparcity/suitesparse/mtx/rcm/"
+
+# List of input matrices
+INPUT_MATRICES=("AMD/G3_circuit.mtx.gz"
+"Bodendiek/CurlCurl_4.mtx.gz"
+"DIMACS10/333SP.mtx.gz"
+"DIMACS10/adaptive.mtx.gz"
+"DIMACS10/AS365.mtx.gz"
+"DIMACS10/delaunay_n24.mtx.gz"
+"DIMACS10/europe_osm.mtx.gz"
+"DIMACS10/hugebubbles-00020.mtx.gz"
+"DIMACS10/hugetrace-00020.mtx.gz"
+"DIMACS10/hugetric-00020.mtx.gz"
+"DIMACS10/kron_g500-logn21.mtx.gz"
+"DIMACS10/M6.mtx.gz"
+"DIMACS10/NLR.mtx.gz"
+"DIMACS10/rgg_n_2_24_s0.mtx.gz"
+"DIMACS10/road_usa.mtx.gz"
+"DIMACS10/venturiLevel3.mtx.gz"
+"Fluorem/HV15R.mtx.gz"
+"Freescale/circuit5M.mtx.gz"
+"GAP/GAP-road.mtx.gz"
+"GenBank/kmer_V2a.mtx.gz"
+"Gleich/wb-edu.mtx.gz"
+"Janna/Queen_4147.mtx.gz"
+"LAW/uk-2005.mtx.gz"
+"MAWI/mawi_201512020000.mtx.gz"
+"Mycielski/mycielskian19.mtx.gz"
+"Pajek/patents.mtx.gz"
+"Rajat/rajat31.mtx.gz"
+"Schenk_AFE/af_shell10.mtx.gz"
+"Schenk/nlpkkt240.mtx.gz"
+"SNAP/soc-LiveJournal1.mtx.gz"
+"SNAP/com-LiveJournal.mtx.gz"
+"SNAP/cit-Patents.mtx.gz"
+"SNAP/sx-stackoverflow.mtx.gz"
+"SNAP/wiki-Talk.mtx.gz"
+"SNAP/roadNet-CA.mtx.gz"
+"SNAP/wiki-topcats.mtx.gz"
+"SNAP/as-Skitter.mtx.gz"
+"vanHeukelum/cage15.mtx.gz"
+"VLSI/stokes.mtx.gz"
+"Zaoui/kkt_power.mtx.gz"
+)    
+
+mkdir -p /work/$USER/tmp10  
+
+for input_matrix in "${INPUT_MATRICES[@]}"; do
+
+    # Extract the base file name (without path or extension)
+    base_filename=$(basename "$input_matrix" .gz)
+
+    # Concatenate path with matrix name
+    path_to_input_matrix="$MATRIX_DIR$input_matrix"
+
+    # Decompress the input matrix
+    gunzip -c "$path_to_input_matrix" > "/work/santiago/tmp10/$base_filename"
+    
+    # Create directory if it doesn't exist
+    mkdir -p "reports"
+
+    # Run the program with the input matrix
+    srun ncu -o "reports/report_spmm_1D_rcm_$base_filename" -k spmm_1D -c 1 --set full ./row_per_thread "/work/santiago/tmp10/$base_filename"
+
+    # Clean up the temporary decompressed file
+    rm "/work/santiago/tmp10/$base_filename"
+
+done
+
+
